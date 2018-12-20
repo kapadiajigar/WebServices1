8dd8a075729681f4a385af486ee5243ea3c5695e
Jigar Kapadia <jbk93i@gmail.com>
2018-12-20 11:44:45 -0500

    Initial commit

---
+++ src/services/CarService.java
@@ -0,0 +1,35 @@
+package services;
+
+public class CarService {
+
+    private int id;
+    private String carName;
+
+    public CarService(int id, String carName) {
+        this.id = id;
+        this.carName = carName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getResults(int id){
+
+        return "Hello";
+    }
+
+
+}
